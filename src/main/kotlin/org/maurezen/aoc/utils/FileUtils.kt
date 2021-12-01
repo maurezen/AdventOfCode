ef6894989e20f6c9ac71501dc5e5aee1a903ad2f
maurezen <maurezen@gmail.com>
2021-12-02 00:18:43 +0300

    utilities for reading the input

---
+++ src/main/kotlin/org/maurezen/aoc/utils/FileUtils.kt
@@ -0,0 +1,24 @@
+package org.maurezen.aoc.utils
+
+import java.io.File
+import java.util.function.Function
+
+class Locator {}
+
+/**
+ * Reads file from resources
+ */
+fun readFile(name: String): List<String> {
+    return readStuffFromFile(name) { it }
+}
+
+fun readNumbersFromFile(name: String): List<Int> {
+    return readStuffFromFile(name) { Integer.parseInt(it) }
+}
+
+fun <T> readStuffFromFile(name: String, transformer: Function<String, T>): List<T> {
+    val path = Locator::class.java.getResource(name).path
+    val list = mutableListOf<T>()
+    File(path).useLines { sequence -> sequence.forEach { list.add(transformer.apply(it)) } }
+    return list
+}
