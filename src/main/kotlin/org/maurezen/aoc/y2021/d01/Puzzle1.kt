2004bb76cbe815bb3b8c872718ef0a946700d9b9
maurezen <maurezen@gmail.com>
2021-12-02 10:21:29 +0300

    day 01 puzzles rearranged

--- src/main/kotlin/org/maurezen/aoc/y2021/d01/Puzzle1.kt
+++ src/main/kotlin/org/maurezen/aoc/y2021/d01/Puzzle1.kt
@@ -3,7 +3,7 @@ package org.maurezen.aoc.y2021.d01
 import org.maurezen.aoc.utils.readNumbersFromFile
 import org.maurezen.aoc.NumericPuzzle
 
-class Puzzle1: NumericPuzzle<Int> {
+open class Puzzle1: NumericPuzzle<Int> {
     override fun run(input: List<Int>): Int {
         return input.zipWithNext().filter { it.second > it.first }.count()
     }
--- src/main/kotlin/org/maurezen/aoc/y2021/d01/Puzzle2.kt
+++ src/main/kotlin/org/maurezen/aoc/y2021/d01/Puzzle2.kt
@@ -1,16 +1,10 @@
 package org.maurezen.aoc.y2021.d01
 
-import org.maurezen.aoc.NumericPuzzle
-
-class Puzzle2: NumericPuzzle<Int> {
+class Puzzle2: Puzzle1() {
     override fun run(input: List<Int>): Int {
         return input
             .windowed(3, 1, false, List<Int>::sum)
             .zipWithNext().filter { it.second > it.first }.count()
-    }
-
-    override fun inputName(): String {
-        return "/input01-2"
     }
 }
 
