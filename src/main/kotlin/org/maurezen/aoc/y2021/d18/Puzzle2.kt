d7b6f2f27bb00e273ff1848b7bc3b3d0c9115a67
maurezen <maurezen@gmail.com>
2021-12-19 17:29:58 +0300

    day 18 puzzle 2 trivial after we got immutability

---
+++ src/main/kotlin/org/maurezen/aoc/y2021/d18/Puzzle2.kt
@@ -0,0 +1,18 @@
+package org.maurezen.aoc.y2021.d18
+
+class Puzzle2: Puzzle1() {
+
+    override fun run(input: List<SnailNumber>): Int {
+        return (input.indices x input.indices)
+            .filterNot { it.first == it.second }
+            .maxOf { (input[it.first] + input[it.second]).magnitude() }
+    }
+}
+
+private infix fun <V, U> Iterable<V>.x(another: Iterable<U>): Iterable<Pair<V, U>> {
+    return this.map { first -> another.map { Pair(first, it) } }.flatten()
+}
+
+fun main() {
+    Puzzle2().solve()
+}
